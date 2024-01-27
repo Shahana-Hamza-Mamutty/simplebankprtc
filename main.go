64a9fbe651c0c701c283340d6064ce9e6efa44ca
Shahana Hamza Mamutty <shahanamamutty@gmail.com>
2024-01-27 15:59:23 +0400

    Added gin and viper

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"simplebank/api"
+	db "simplebank/db/sqlc"
+	"simplebank/util"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config")
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("Cannot connect to database", err)
+	}
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+	err = server.Start(config.HTTPServerAddress)
+	if err != nil {
+		log.Fatal("Cannot start server", err)
+	}
+}
